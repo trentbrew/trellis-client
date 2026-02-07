51ea0a750d5dcb8f2da8a2f785fe7e5f15858486
trentbrew <hello@trentbrew.com>
2026-02-06 23:08:31 -0800

    chore(auto): backup at 2026-02-06 23:08:31 [skip ci]

--- apps/web/app/layouts/default.vue
+++ apps/web/app/layouts/default.vue
@@ -178,14 +178,14 @@
     <IconRail />
 
     <div class="flex flex-1 flex-col min-w-0 overflow-hidden">
-      <AppHeader />
       <div class="flex flex-1 min-h-0 overflow-hidden">
         <AppSidebar />
         <main
-          ref="pageEl"
-          class="page-transition-wrapper bg-transparent flex-1 overflow-y-auto p-0 relative"
-          aria-label="Main content">
-          <slot />
+        ref="pageEl"
+        class="page-transition-wrapper bg-transparent flex-1 overflow-y-auto p-0 relative"
+        aria-label="Main content">
+        <AppHeader />
+        <slot />
         </main>
       </div>
     </div>
