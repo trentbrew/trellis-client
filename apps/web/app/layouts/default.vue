38a9ddd5d0a6699593249211192a704847ada341
trentbrew <hello@trentbrew.com>
2026-02-06 23:29:34 -0800

    chore(auto): backup at 2026-02-06 23:29:34 [skip ci]

--- apps/web/app/layouts/default.vue
+++ apps/web/app/layouts/default.vue
@@ -180,13 +180,15 @@
     <div class="flex flex-1 flex-col min-w-0 overflow-hidden">
       <div class="flex flex-1 min-h-0 overflow-hidden">
         <AppSidebar />
-        <main
-        ref="pageEl"
-        class="page-transition-wrapper bg-transparent flex-1 overflow-y-auto p-0 relative"
-        aria-label="Main content">
-        <AppHeader />
-        <slot />
-        </main>
+        <div class="flex flex-1 min-h-0 overflow-hidden">
+          <AppHeader />
+          <main
+          ref="pageEl"
+          class="page-transition-wrapper bg-transparent flex-1 overflow-y-auto p-0 relative"
+          aria-label="Main content">
+          <slot />
+          </main>
+        </div>
       </div>
     </div>
 
